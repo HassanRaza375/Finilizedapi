391fa8cad4a11f8457ed4e182675b444882bbed2
Syed Hassan Raza <105318011+HassanRaza375@users.noreply.github.com>
2023-06-25 23:48:39 +0500

    Updates

--- api/routes/product.js
+++ api/routes/product.js
@@ -1,28 +1,92 @@
 const express = require("express");
 const router = express.Router();
+const Producted = require("../module/product");
+const mongoose = require("mongoose");
 router.get("/", (req, res, next) => {
-  res.status(200).json({ msg: "Handling get /product request" });
+  Producted.find()
+    .select("name price _id")
+    .exec()
+    .then((e) => {
+      const response = {
+        count: e.length,
+        Products: e,
+      };
+      res
+        .status(200)
+        .json({ msg: "Handling get /product request", Response: response });
+    })
+    .catch((e) => {
+      res.status(404).json({
+        Err: e,
+        Files: "not found",
+      });
+    });
 });
 
 router.get("/:ID", (req, res, next) => {
   const id = req.params.ID;
-  if (id === "special") {
-    res
-      .status(200)
-      .json({ msg: "Congratulation you have found a unique ID", ID: id });
-  } else {
-    res.status(200).json({ msg: "Passed an ID", ID: id });
-  }
+  Producted.findById(id)
+    .exec()
+    .then((e) => {
+      if (e) {
+        console.log(e);
+        res.status(200).json(e);
+      } else {
+        res.status(404).json({ message: "not found" });
+      }
+    })
+    .catch((e) => {
+      console.log(e);
+      res.status(500).json({ err: "Not found", message: e });
+    });
 });
 
 router.post("/", (req, res, next) => {
-  res.status(200).json({ msg: "Handling post /product request" });
+  debugger;
+  const products = new Producted({
+    _id: new mongoose.Types.ObjectId(),
+    name: req.body.name,
+    price: req.body.price,
+  });
+  products
+    .save()
+    .then((res) => {
+      console.log(res);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  res
+    .status(200)
+    .json({ msg: "Handling post /product request", pro: products });
 });
 router.patch("/:ID", (req, res, next) => {
-  res.status(200).json({ msg: "Handling patch /product request" });
+  const id = req.params.ID;
+  console.log(id);
+  const updateops = {};
+  for (const ops of req.body) {
+    updateops[ops.propName] = ops.value;
+  }
+  // { name: req.body.newname, price: req.body.newprice }
+  Producted.findOneAndUpdate({ _id: id }, { $set: updateops })
+    .exec()
+    .then((e) => {
+      res.status(200).json({ message: e });
+    })
+    .catch((e) => {
+      console.log(e);
+    });
 });
 router.delete("/:ID", (req, res, next) => {
-  res.status(200).json({ msg: "Handling delete /product request" });
+  const id = req.params.ID;
+  Producted.deleteOne({ _id: id })
+    .exec()
+    .then((e) => {
+      res.status(200).json({ message: e });
+    })
+    .catch((e) => {
+      res.status(404).json({ message: e });
+    });
 });
 
 module.exports = router;
--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 const productroutes = require("./api/routes/product");
 const orderroutes = require("./api/routes/Order");
+// body parser to get data 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const mongoose = require("mongoose");
+mongoose.connect(
+  "mongodb+srv://hassanraza375:U7uYsL54njaa1OOw@cluster0.tgn1ggl.mongodb.net/?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 app.use("/product", productroutes);
 app.use("/order", orderroutes);
 
